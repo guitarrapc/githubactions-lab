c73a58255949bbeb83a7d08d9261ba38fdd5aa4e
Ikiru Yoshizaki <3856350+guitarrapc@users.noreply.github.com>
2020-04-21 12:13:00 +0900

    chore: add dotnet sample source

---
+++ src/dotnet/console/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Hello World! {args}");
+        }
+    }
+
+    public class Hoge
+    {
+        public string Name { get; set; }
+    }
+}
