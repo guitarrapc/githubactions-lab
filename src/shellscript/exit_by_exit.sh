fec18b54c9b199300594c1dc1ff99a9c9c2a3cc7
Ikiru Yoshizaki <3856350+guitarrapc@users.noreply.github.com>
2020-07-03 12:46:05 +0900

    chore: true

--- src/shellscript/exit_by_exit.sh
+++ src/shellscript/exit_by_exit.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -eux
 
 echo "exit should result error on GitHub Actions."
 exit 1
--- src/shellscript/exit_by_return.sh
+++ src/shellscript/exit_by_return.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -eux
 
 echo "return should result error on GitHub Actions."
 return 1
