32b3cbd87b878a59e6686cd1c43d73f711e4ed56
Ikiru Yoshizaki <3856350+guitarrapc@users.noreply.github.com>
2021-07-09 16:00:20 +0900

    chore: move fail tests

--- src/dotnet/console-fail-tests/UnitTest1.cs
+++ src/dotnet/console-fail-tests/UnitTest1.cs
@@ -6,9 +6,14 @@ namespace console_fail_test
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
-        {
+        public void Test1() => Assert.True(1 == 1);
 
-        }
+        [Fact]
+        public void Test2() => Assert.True(1 == 2, "Let's Fail!!");
+        private void ThrowException() => throw new InvalidOperationException("test fail");
+        [Fact]
+        public void Test3() => ThrowException();
+        [Fact]
+        public void Test4() => Assert.Contains(new[] { 1, 2, 3 }, i => i == 4);
     }
 }
--- src/dotnet/console-tests/UnitTest1.cs
+++ src/dotnet/console-tests/UnitTest1.cs
@@ -15,13 +15,5 @@ namespace console_tests
             };
             Assert.Equal("hoge", hoge.Name);
         }
-
-        [Fact]
-        public void Test2() => Assert.True(1 == 2, "Let's Fail!!");
-        private void ThrowException() => throw new InvalidOperationException("test fail");
-        [Fact]
-        public void Test3() => ThrowException();
-        [Fact]
-        public void Test4() => Assert.Contains(new[] { 1, 2, 3 }, i => i == 4);
     }
 }
